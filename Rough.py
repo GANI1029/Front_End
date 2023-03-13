91bd0db5dc0665b4a7912abe84ff9b628252db4f
GANESH RAJA MANDALA <87810887+GANI1029@users.noreply.github.com>
2023-03-13 20:11:29 +0530

    Update Rough.py

--- Rough.py
+++ Rough.py
@@ -48,3 +48,41 @@ driver.save_screenshot("homepage.png")
 
 # Close the browser
 driver.quit()
+######################---------------
+from PerfectoLabUtils import PerfectoLabUtils
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set your Perfecto Lab credentials here
+user = "{your_username}"
+password = "{your_password}"
+host = "{your_cloud_url}"
+
+# Set up the webdriver to run the tests
+capabilities = {
+    'platformName': 'iOS',
+    'securityToken': '{}:{}'.format(user, password),
+    'model': '{your_ios_device_model}',
+    'browserName': 'mobileOS',
+    'version': '{your_ios_version}',
+    'automationName': 'XCUITest',
+}
+
+# Connect to the Perfecto Lab
+driver = webdriver.Remote('https://' + host + '/nexperience/perfectomobile/wd/hub', capabilities)
+
+# Navigate to the Messages app
+driver.get('sms:')
+# Wait for the Messages app to load
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "table")))
+
+# Find the list of messages and print their contents
+messages = driver.find_elements_by_xpath('//table[@id="table"]//div[@class="item-main-content"]/div[@class="message-text"]')
+for message in messages:
+    print(message.text)
+
+# Close the driver
+driver.quit()
+
